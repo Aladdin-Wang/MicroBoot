e37ae9fd371ed1ffee634864fee8cc14c2600d54
KK <17630350805@163.com>
2024-11-28 13:17:09 +0800

    更新STM32 FLASH驱动

--- source/flash_blob/flash/STM32_FLASH_DEV.c
+++ source/flash_blob/flash/STM32_FLASH_DEV.c
@@ -1,25 +1,5 @@
 #include "../flash_blob.h"
 
-#ifdef STM32F10x_16
-#include "stm32f1xx.h"
-static struct FlashDevice const FlashDevice  =  {
-   FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F10x Low-density Flash", // Device Name (16kB)
-   ONCHIP,                     // Device Type
-   0x08000000,                 // Device Start Address
-   0x00004000,                 // Device Size in Bytes (16kB)
-   1024,                       // Programming Page Size
-   0,                          // Reserved, must be 0
-   0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
-
-// Specify Size and Address of Sectors
-   0x0400, 0x000000,           // Sector Size 1kB (128 Sectors)
-   SECTOR_END
-};
-#endif
-
 #ifdef STM32F10x_128
 #include "stm32f1xx.h"
 static struct FlashDevice const FlashDevice  =  {
@@ -41,8 +21,7 @@ static struct FlashDevice const FlashDevice  =  {
 #endif
 
 #ifdef STM32F10x_512
-#include "stm32f1xx.h"
-static struct FlashDevice const FlashDevice  =  {
+struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
    "STM32F10x High-density Flash",// Device Name (512kB/384kB/256kB)
    ONCHIP,                     // Device Type
@@ -80,93 +59,33 @@ static struct FlashDevice const FlashDevice  =  {
 };
 #endif
 
-#ifdef STM32F10x_CL
-#include "stm32f1xx.h"
+#ifdef STM32F2xx_1024
+#include "stm32f2xx.h"
 static struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F10x Connectivity Line Flash",// Device Name (256kB/128kB/64kB)
+   "STM32F2xx Flash",          // Device Name (1024kB/768kB/512kB/256kB/128kB)
    ONCHIP,                     // Device Type
    0x08000000,                 // Device Start Address
-   0x00040000,                 // Device Size in Bytes (256kB)
+   0x00100000,                 // Device Size in Bytes (1024kB)
    1024,                       // Programming Page Size
    0,                          // Reserved, must be 0
    0xFF,                       // Initial Content of Erased Memory
    100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
+   6000,                       // Erase Sector Timeout 6000 mSec
 
 // Specify Size and Address of Sectors
-   0x0800, 0x000000,           // Sector Size 2kB (128 Sectors)
+   0x04000, 0x000000,          // Sector Size  16kB (4 Sectors)
+   0x10000, 0x010000,          // Sector Size  64kB (1 Sectors)
+   0x20000, 0x020000,          // Sector Size 128kB (7/5/3 Sectors)
    SECTOR_END
 };
-#endif
-
-#ifdef STM32F103xE
-
-#include "stm32f1xx.h"
-static struct FlashDevice const FlashDevice  =  {
-   FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F10x Flash",// Device Name (256kB/128kB/64kB)
-   ONCHIP,                     // Device Type
-   0x08000000,                 // Device Start Address
-   0x00080000,                 // Device Size in Bytes (64kB)
-   1024,                       // Programming Page Size
-   0,                          // Reserved, must be 0
-   0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
-
-// Specify Size and Address of Sectors
-   0x0800, 0x000000,           // Sector Size 2kB (128 Sectors)
-   SECTOR_END
-};
-#endif
-
-#ifdef STM32F105xC
-
-#include "stm32f1xx.h"
-static struct FlashDevice const FlashDevice  =  {
-   FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F10x Flash",// Device Name (256kB/128kB/64kB)
-   ONCHIP,                     // Device Type
-   0x08000000,                 // Device Start Address
-   0x00040000,                 // Device Size in Bytes (64kB)
-   1024,                       // Programming Page Size
-   0,                          // Reserved, must be 0
-   0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
-
-// Specify Size and Address of Sectors
-   0x0800, 0x000000,           // Sector Size 2kB (128 Sectors)
-   SECTOR_END
-};
-#endif
-
-#ifdef STM32F303x8
-#include "stm32f1xx.h"
-static struct FlashDevice const FlashDevice  =  {
-   FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F30x Flash",// Device Name (256kB/128kB/64kB)
-   ONCHIP,                     // Device Type
-   0x08000000,                 // Device Start Address
-   0x00010000,                 // Device Size in Bytes (64kB)
-   1024,                       // Programming Page Size
-   0,                          // Reserved, must be 0
-   0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
-
-// Specify Size and Address of Sectors
-   0x0800, 0x000000,           // Sector Size 2kB (128 Sectors)
-   SECTOR_END
-};
-#endif
+#endif // STM32F2xx_1024
 
 #ifdef STM32F4xx_1024
 #include "stm32f4xx.h"
 static struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32F4xx 1MB Flash",// Device Name (256kB/128kB/64kB)
+   "STM32F4xx 1MB Flash",      // Device Name (256kB/128kB/64kB)
    ONCHIP,                     // Device Type
    0x08000000,                 // Device Start Address
    0x00100000,                 // Device Size in Bytes (512kB)
@@ -183,26 +102,94 @@ static struct FlashDevice const FlashDevice  =  {
    SECTOR_END
 };
 #endif
-#ifdef STM32G431xx
+
+#if defined STM32F4xx_1024dual
+  struct FlashDevice const FlashDevice  =  {
+    FLASH_DRV_VERS,             // Driver Version, do not modify!
+    "STM32F4xx 1MB dual bank Flash",     // Device Name
+    ONCHIP,                     // Device Type
+    0x08000000,                 // Device Start Address
+    0x00100000,                 // Device Size in Bytes (1024kB)
+    1024,                       // Programming Page Size
+    0,                          // Reserved, must be 0
+    0xFF,                       // Initial Content of Erased Memory
+    1000,                       // Program Page Timeout 1000 mSec
+    6000,                       // Erase Sector Timeout 6000 mSec
+    // Specify Size and Address of Sectors
+    0x04000, 0x000000,          // Sector Size  16kB (4 Sectors)
+    0x10000, 0x010000,          // Sector Size  64kB (1 Sectors)
+    0x20000, 0x020000,          // Sector Size 128kB (3 Sectors)
+    0x04000, 0x080000,          // Sector Size  16kB (4 Sectors)
+    0x10000, 0x090000,          // Sector Size  64kB (1 Sectors)
+    0x20000, 0x0A0000,          // Sector Size 128kB (3 Sectors)
+    SECTOR_END
+  };
+#endif // STM32F7x_1024dual
+
+#ifdef STM32F4xx_1536
+  struct FlashDevice const FlashDevice  =  {
+    FLASH_DRV_VERS,             // Driver Version, do not modify!
+    "STM32F4xx 1.5MB Flash",      // Device Name (1024kB/1536kB)
+    ONCHIP,                     // Device Type
+    0x08000000,                 // Device Start Address
+    0x00180000,                 // Device Size in Bytes (1536kB)
+    1024,                       // Programming Page Size
+    0,                          // Reserved, must be 0
+    0xFF,                       // Initial Content of Erased Memory
+    100,                        // Program Page Timeout 100 mSec
+    6000,                       // Erase Sector Timeout 6000 mSec
+    // Specify Size and Address of Sectors
+    0x04000, 0x000000,          // Sector Size  16kB (4 Sectors)
+    0x10000, 0x010000,          // Sector Size  64kB (1 Sectors)
+    0x20000, 0x020000,          // Sector Size 128kB (11 Sectors)
+    SECTOR_END
+  };
+#endif // STM32F2xx_1536
+
+#ifdef STM32F4xx_2048
+  struct FlashDevice const FlashDevice  =  {
+    FLASH_DRV_VERS,             // Driver Version, do not modify!
+    "STM32F4xx 2MB Flash",      // Device Name (2048kB)
+    ONCHIP,                     // Device Type
+    0x08000000,                 // Device Start Address
+    0x00200000,                 // Device Size in Bytes (2048kB)
+    1024,                       // Programming Page Size
+    0,                          // Reserved, must be 0
+    0xFF,                       // Initial Content of Erased Memory
+    100,                        // Program Page Timeout 100 mSec
+    6000,                       // Erase Sector Timeout 6000 mSec
+    // Specify Size and Address of Sectors
+    0x04000, 0x000000,          // Sector Size  16kB (4 Sectors)
+    0x10000, 0x010000,          // Sector Size  64kB (1 Sectors)
+    0x20000, 0x020000,          // Sector Size 128kB (7 Sectors)
+    0x04000, 0x100000,          // Sector Size  16kB (4 Sectors)
+    0x10000, 0x110000,          // Sector Size  64kB (1 Sectors)
+    0x20000, 0x120000,          // Sector Size 128kB (7 Sectors)
+    SECTOR_END
+  };
+#endif // STM32F2xx_2048
+  
+  
+#ifdef STM32G4xx_512
 #include "stm32g4xx.h"
 static struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
-   "STM32G4xx 128KB Flash",// Device Name (256kB/128kB/64kB)
+   "STM32G4xx 512KB",          // Device Name (256kB/128kB/64kB)
    ONCHIP,                     // Device Type
    0x08000000,                 // Device Start Address
-   0x00020000,                 // Device Size in Bytes (1024kB)
+   0x00080000,                 // Device Size in Bytes (512kB)
    1024,                       // Programming Page Size
    0,                          // Reserved, must be 0
    0xFF,                       // Initial Content of Erased Memory
-   100,                        // Program Page Timeout 100 mSec
-   500,                        // Erase Sector Timeout 500 mSec
+   400,                        // Program Page Timeout 400 mSec
+   400,                        // Erase Sector Timeout 400 mSec
 
 // Specify Size and Address of Sectors
    0x0800, 0x00000000,
    SECTOR_END
 };
 #endif
-#ifdef STM32H743xx
+#ifdef STM32H7x_2048
 #include "stm32h7xx.h"
 static struct FlashDevice const FlashDevice  =  {
    FLASH_DRV_VERS,             // Driver Version, do not modify!
--- source/flash_blob/flash/STM32_FLASH_DRV.c
+++ source/flash_blob/flash/STM32_FLASH_DRV.c
@@ -1,14 +1,19 @@
 #include "../flash_blob.h"
 #include "STM32_FLASH_DEV.c"
 
-#if defined(STM32F103xB)||defined(STM32F103xE) || defined(STM32F105xC)
-    typedef volatile unsigned char  vu8;
-    typedef volatile unsigned long  vu32;
-    typedef volatile unsigned short vu16;
+typedef volatile unsigned char    vu8;
+typedef          unsigned char     u8;
+typedef volatile unsigned short   vu16;
+typedef          unsigned short    u16;
+typedef volatile unsigned long    vu32;
+typedef          unsigned long     u32;
 
-    #define M8(adr)  (*((vu8  *) (adr)))
-    #define M16(adr) (*((vu16 *) (adr)))
-    #define M32(adr) (*((vu32 *) (adr)))
+#define M8(adr)  (*((vu8  *) (adr)))
+#define M16(adr) (*((vu16 *) (adr)))
+#define M32(adr) (*((vu32 *) (adr)))
+	
+#if defined(STM32F10x_128)||defined(STM32F10x_512)||defined(STM32F10x_1024)
+
     // FLASH BANK size
     #define BANK1_SIZE      0x08080000      // Bank1 Size = 512kB
 
@@ -26,38 +31,70 @@
     #define FLASH_PGERR     0x00000004
     #define FLASH_WRPRTERR  0x00000010
     #define FLASH_EOP       0x00000020
-
 #endif
-#if defined(STM32F303x8)
-    typedef volatile unsigned short   vu16;
-    typedef          unsigned short    u16;
-    typedef volatile unsigned long    vu32;
-    typedef          unsigned long     u32;
 
-    #define M16(adr) (*((vu16 *) (adr)))
-    #define M32(adr) (*((vu32 *) (adr)))
+#if defined(STM32F2xx_1024)
+	// Flash Control Register definitions
+	#define FLASH_PG                ((unsigned int)0x00000001)
+	#define FLASH_SER               ((unsigned int)0x00000002)
+	#define FLASH_MER               ((unsigned int)0x00000004)
+	#define FLASH_SNB_POS           ((unsigned int)0x00000003)
+	#define FLASH_SNB_MSK           ((unsigned int)0x00000078)
+	#define FLASH_PSIZE_POS         ((unsigned int)0x00000008)
+	#define FLASH_PSIZE_MSK         ((unsigned int)0x00000300)
+	#define FLASH_STRT              ((unsigned int)0x00010000)
+	#define FLASH_LOCK              ((unsigned int)0x80000000)
 
-    // Flash Control Register definitions
-    #define FLASH_PG                ((unsigned int)(1ul << 0))
-    #define FLASH_PER               ((unsigned int)(1ul << 1))
-    #define FLASH_MER               ((unsigned int)(1ul << 2))
-    #define FLASH_OPTPG             ((unsigned int)(1ul << 4))
-    #define FLASH_OPTER             ((unsigned int)(1ul << 5))
-    #define FLASH_STRT              ((unsigned int)(1ul << 6))
-    #define FLASH_LOCK              ((unsigned int)(1ul << 7))
-    #define FLASH_OPTWRE            ((unsigned int)(1ul << 9))
+	// Flash Option Control Register definitions
+	#define FLASH_OPTLOCK           ((unsigned int)0x00000001)
+	#define FLASH_OPTSTRT           ((unsigned int)0x00000002)
 
-    // Flash Status Register definitions
-    #define FLASH_BSY               ((unsigned int)(1ul << 0))
-    #define FLASH_PGERR             ((unsigned int)(1ul << 2))
-    #define FLASH_WRPRTERR          ((unsigned int)(1ul << 4))
-    #define FLASH_EOP               ((unsigned int)(1ul << 5))
 
-    #define FLASH_ERR               (FLASH_PGERR | FLASH_WRPRTERR)
+	#define FLASH_PSIZE_Byte        ((unsigned int)0x00000000)
+	#define FLASH_PSIZE_HalfWord    ((unsigned int)0x00000100)
+	#define FLASH_PSIZE_Word        ((unsigned int)0x00000200)
+	#define FLASH_PSIZE_DoubleWord  ((unsigned int)0x00000300)
+
+
+	// Flash Status Register definitions
+	#define FLASH_EOP               ((unsigned int)0x00000001)
+	#define FLASH_OPERR             ((unsigned int)0x00000002)
+	#define FLASH_WRPERR            ((unsigned int)0x00000010)
+	#define FLASH_PGAERR            ((unsigned int)0x00000020)
+	#define FLASH_PGPERR            ((unsigned int)0x00000040)
+	#define FLASH_PGSERR            ((unsigned int)0x00000080)
+	#define FLASH_BSY               ((unsigned int)0x00010000)
+
+	#define FLASH_PGERR             (FLASH_PGSERR | FLASH_PGPERR | FLASH_PGAERR | FLASH_WRPERR)
+	
+/*
+ * Get Sector Number
+ *    Parameter:      adr:  Sector Address
+ *    Return Value:   Sector Number
+ */
+
+unsigned long GetSecNum (unsigned long adr) {
+  unsigned long n;
+
+  n = (adr >> 12) & 0x000FF;                             // only bits 12..19
+
+  if    (n >= 0x20) {
+    n = 4 + (n >> 5);                                    // 128kB Sector
+  }
+  else if (n >= 0x10) {
+    n = 3 + (n >> 4);                                    //  64kB Sector
+  }
+  else                {
+    n = 0 + (n >> 2);                                    //  16kB Sector
+  }
+
+  return (n);                                            // Sector Number
+}
+	
 #endif
-#if defined(STM32G431xx)
-typedef volatile unsigned long    vu32;
-typedef          unsigned long     u32;
+
+#if defined(STM32G4xx_512)
+
 #define M32(adr) (*((vu32 *) (adr)))
 #define FLASH_SR_MISSERR        ((u32)(  1U <<  8))
 static void DSB(void)
@@ -142,18 +179,8 @@ static u32 GetFlashBankNum(u32 adr)
     return (flashBankNum);
 }
 #endif
-#if defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
-    typedef volatile unsigned char    vu8;
-    typedef          unsigned char     u8;
-    typedef volatile unsigned short   vu16;
-    typedef          unsigned short    u16;
-    typedef volatile unsigned long    vu32;
-    typedef          unsigned long     u32;
+#if defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
 
-    #define M8(adr)  (*((vu8  *) (adr)))
-    #define M16(adr) (*((vu16 *) (adr)))
-    #define M32(adr) (*((vu32 *) (adr)))
     // Flash Keys
 #if !defined  (USE_HAL_DRIVER)		
     #define FLASH_KEY1      0x45670123
@@ -196,7 +223,7 @@ static u32 GetFlashBankNum(u32 adr)
     #define FLASH_PGERR             (FLASH_PGSERR | FLASH_PGPERR | FLASH_PGAERR | FLASH_WRPERR)
 
 #endif
-#if defined(STM32H743xx)
+#if defined(STM32H7x_2048)
 typedef volatile unsigned long    vu32;
 typedef          unsigned long     u32;
 
@@ -257,15 +284,14 @@ static int32_t UnInit(uint32_t fnc)
 
 static void flash_unlock()
 {
-    #if defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
+    #if defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
     FLASH->KEYR = FLASH_KEY1;                             // Unlock Flash
     FLASH->KEYR = FLASH_KEY2;
 
     FLASH->ACR &= 0x0000000F;                             // keep Wait States, no Cache, no Prefetch
     FLASH->SR  |= FLASH_PGERR;                            // Reset Error Flags
 
-    #elif defined(STM32G431xx)
+    #elif defined(STM32G4xx_512)
     FLASH->KEYR = FLASH_KEY1;                              /* Unlock Flash operation */
     FLASH->KEYR = FLASH_KEY2;
 
@@ -273,7 +299,7 @@ static void flash_unlock()
     while (FLASH->SR & FLASH_SR_BSY);
 
 
-    #elif defined (STM32F103xB) || defined (STM32F103xE) || defined(STM32F105xC)
+    #elif defined(STM32F10x_128)||defined(STM32F10x_512)||defined(STM32F10x_1024)
     // Unlock Flash
     FLASH->KEYR  = FLASH_KEY1;
     FLASH->KEYR  = FLASH_KEY2;
@@ -282,10 +308,12 @@ static void flash_unlock()
     FLASH->KEYR2 = FLASH_KEY1;                    // Flash bank 2
     FLASH->KEYR2 = FLASH_KEY2;
     #endif
-    #elif defined(STM32F303x8)
+    #elif defined(STM32F2xx_1024)
     FLASH->KEYR  = FLASH_KEY1;
     FLASH->KEYR  = FLASH_KEY2;
-    #elif defined(STM32H743xx)
+	FLASH->ACR &= 0x0000000F;                             // keep Wait States, no Cache, no Prefetch
+	FLASH->SR  |= FLASH_PGERR;                            // Reset Error Flags
+    #elif defined(STM32H7x_2048)
     FLASH->KEYR1 = FLASH_KEY1;                     /* Unlcock FLASH A Registers access */
     FLASH->KEYR1 = FLASH_KEY2;
     FLASH->CCR1  = FLASH_PGERR;                    /* Clear status register  */
@@ -298,20 +326,19 @@ static void flash_unlock()
 
 static void flash_lock()
 {
-    #if defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
+    #if defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
     FLASH->CR |=  FLASH_LOCK;                             // Lock Flash
-    #elif defined(STM32G431xx)
+    #elif defined(STM32G4xx_512)
     FLASH->CR |= FLASH_CR_LOCK;                            /* Lock Flash operation */
     DSB();
-    #elif defined (STM32F103xB) || defined (STM32F103xE) || defined(STM32F105xC)
+    #elif defined(STM32F10x_128)||defined(STM32F10x_512)||defined(STM32F10x_1024)
     FLASH->CR  |=  FLASH_LOCK;
     #ifdef STM32F10x_1024
     FLASH->CR2 |=  FLASH_LOCK;                    // Flash bank 2
     #endif
-    #elif defined(STM32F303x8)
+    #elif defined(STM32F2xx_1024)
     FLASH->CR |=  FLASH_LOCK;                             // Lock Flash
-    #elif defined (STM32H743xx)
+    #elif defined (STM32H7x_2048)
     FLASH->CR1 |=  FLASH_CR_LOCK;                  /* Lock FLASH A Registers access */
     FLASH->CR2 |=  FLASH_CR_LOCK;                  /* Lock FLASH B Registers access */
     #endif	
@@ -325,8 +352,7 @@ static void flash_lock()
 static int32_t EraseChip(void)
 {
 	flash_unlock();
-    #if defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
+    #if defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
     FLASH->CR |=  FLASH_MER;                              // Mass Erase Enabled (sectors  0..11)
     #ifdef STM32F4xx_2048
     FLASH->CR |=  FLASH_MER1;                             // Mass Erase Enabled (sectors 12..23)
@@ -343,7 +369,7 @@ static int32_t EraseChip(void)
     FLASH->CR &= ~FLASH_MER1;                             // Mass Erase Disabled
     #endif
 
-    #elif defined(STM32G431xx)
+    #elif defined(STM32G4xx_512)
     FLASH->SR  = FLASH_PGERR;                              /* Reset Error Flags */
 
     FLASH->CR  = (FLASH_CR_MER1 | FLASH_CR_MER2);          /* Bank A/B mass erase enabled */
@@ -368,7 +394,7 @@ static int32_t EraseChip(void)
 
     FLASH->CR2 &= ~FLASH_MER;
     #endif
-    #elif defined(STM32F303x8)
+    #elif defined(STM32F2xx_1024)
     FLASH->CR |=  FLASH_MER;                              // Mass Erase Enabled
     FLASH->CR |=  FLASH_STRT;                             // Start Erase
 
@@ -376,7 +402,7 @@ static int32_t EraseChip(void)
 
     FLASH->CR &= ~FLASH_MER;                              // Mass Erase Disabled
 
-    #elif defined(STM32H743xx)
+    #elif defined(STM32H7x_2048)
     FLASH->CCR1 = FLASH_PGERR;                     /* Clear status register  */
     FLASH->CR1  = FLASH_CR_BER | FLASH_CR_PSIZE_VAL;
     FLASH->CR1 |= FLASH_CR_START;
@@ -410,7 +436,7 @@ static int32_t EraseSector(uint32_t adr)
 	flash_unlock();
     /*Variable used for Erase procedure*/
 
-    #if  defined (STM32F103xB) || defined(STM32F105xC) || defined(STM32F103xE)
+    #if  defined(STM32F10x_128)||defined(STM32F10x_512)||defined(STM32F10x_1024)
     #ifdef STM32F10x_1024
 
     if (adr < (BANK1_SIZE)) {          // Flash bank 2
@@ -433,16 +459,25 @@ static int32_t EraseSector(uint32_t adr)
 
     #endif
 
-    #elif defined(STM32F303x8)
-    FLASH->CR  |=  FLASH_PER;                           // Page Erase Enabled
-    FLASH->AR   =  adr;                                 // Page Address
-    FLASH->CR  |=  FLASH_STRT;                          // Start Erase
+    #elif defined(STM32F2xx_1024)
+	  unsigned long n;
 
-    while (FLASH->SR  & FLASH_BSY);
+	  n = GetSecNum(adr);                                   // Get Sector Number
 
-    FLASH->CR  &= ~FLASH_PER;                           // Page Erase Disabled
+	  FLASH->SR |= FLASH_PGERR;                             // Reset Error Flags
 
-    #elif defined(STM32G431xx)
+	  FLASH->CR  =  FLASH_SER;                              // Sector Erase Enabled 
+	  FLASH->CR |=  ((n << FLASH_SNB_POS) & FLASH_SNB_MSK); // Sector Number
+	  FLASH->CR |=  FLASH_STRT;                             // Start Erase
+
+      while (FLASH->SR  & FLASH_BSY);
+
+      FLASH->CR &= ~FLASH_SER;                        // Page Erase Disabled
+	  if (FLASH->SR & FLASH_PGERR) {                        // Check for Error
+		FLASH->SR |= FLASH_PGERR;                           // Reset Error Flags
+		result =(1);                                           // Failed
+	  }
+    #elif defined(STM32G4xx_512)
     u32 b, p;
     b = GetFlashBankNum(adr);                              /* Get Bank Number 0..1  */
     p = get_flash_sector(adr);                              /* Get Page Number 0..127 */
@@ -461,8 +496,7 @@ static int32_t EraseSector(uint32_t adr)
         result =(1);                                          /* Failed */
     }
 
-    #elif defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
+    #elif defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
 
     unsigned long n;
 
@@ -482,7 +516,7 @@ static int32_t EraseSector(uint32_t adr)
         result =(1);                                          // Failed
     }
 
-    #elif defined(STM32H743xx)
+    #elif defined(STM32H7x_2048)
     unsigned long b, s;
 
     b = GetBankNum(adr);                           /* get Flash Bank number */
@@ -531,7 +565,7 @@ static int32_t ProgramPage(uint32_t addr, uint32_t sz, uint8_t* buf)
 {
     int32_t result = 0;
 	flash_unlock();
-    #if defined (STM32F103xB) ||defined(STM32F303x8) || defined(STM32F105xC) ||  defined(STM32F103xE)
+    #if defined(STM32F10x_128)||defined(STM32F10x_512)||defined(STM32F10x_1024)
     uint32_t end_addr   = addr + sz;
 
     sz = (sz + 1) & ~1;                           // Adjust size for Half Words
@@ -592,8 +626,34 @@ static int32_t ProgramPage(uint32_t addr, uint32_t sz, uint8_t* buf)
     #endif
 
     #endif
-    #if defined(STM32F407xx) || defined(STM32F427xx) || defined(STM32F437xx) || defined(STM32F429xx)|| \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx) || defined(STM32F413xx)
+    #if defined(STM32F2xx_1024)	
+	  sz = (sz + 3) & ~3;                                   // Adjust size for Words
+	  
+	  FLASH->SR |= FLASH_PGERR;                             // Reset Error Flags
+	  FLASH->CR  =  0;                                      // reset CR 
+
+	  while (sz) {
+		FLASH->CR |= (FLASH_PG              |               // Programming Enabled
+					  FLASH_PSIZE_Word);                    // Programming Enabled (Word)
+
+		M32(addr) = *((u32 *)buf);                           // Program Double Word
+		while (FLASH->SR & FLASH_BSY);
+
+		FLASH->CR &= ~FLASH_PG;                             // Programming Disabled
+
+		if (FLASH->SR & FLASH_PGERR) {                      // Check for Error
+		  FLASH->SR |= FLASH_PGERR;                         // Reset Error Flags
+            result =(1);                                       // Failed
+			break;
+		}
+
+		addr += 4;                                           // Go to next Word
+		buf += 4;
+		sz  -= 4;
+	  }
+    #endif
+	
+    #if defined(STM32F4xx_1024) || defined(STM32F4xx_2048) || defined(STM32F4xx_1536) || defined(STM32F4xx_1024dual)
     sz = (sz + 3) & ~3;                                   // Adjust size for Words
 
     FLASH->SR |= FLASH_PGERR;                             // Reset Error Flags
@@ -620,9 +680,9 @@ static int32_t ProgramPage(uint32_t addr, uint32_t sz, uint8_t* buf)
         sz  -= 4;
     }
 
-
     #endif
-    #if defined(STM32G431xx)//FLASH_TYPEPROGRAM_DOUBLEWORD
+	
+    #if defined(STM32G4xx_512)//FLASH_TYPEPROGRAM_DOUBLEWORD
     sz = (sz + 7) & ~7;                                    /* Adjust size for two words */
 
     FLASH->SR  = FLASH_PGERR;                              /* Reset Error Flags */
@@ -650,7 +710,7 @@ static int32_t ProgramPage(uint32_t addr, uint32_t sz, uint8_t* buf)
     FLASH->CR &= ~(FLASH_CR_PG) ;                          /* Reset CR */
 
     #endif
-    #if defined(STM32H743xx)
+    #if defined(STM32H7x_2048)
     unsigned long b;
     unsigned long *dest_addr = ( u32 *)addr;
     unsigned long *src_addr  = ( u32 *)buf;
