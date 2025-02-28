adc860701de8ee6a2a174907b933e281c0a1ef59
KK <17630350805@163.com>
2025-02-28 11:01:32 +0800

    添加flash驱动模板

---
+++ example/flash/Template_FLASH_DRV.c
@@ -0,0 +1,88 @@
+#include "../flash_algo.h"
+#include "Template_FLASH_DEV.c"
+/*
+ *  Initialize Flash Programming Functions
+ *    Parameter:      adr:  Device Base Address
+ *                    clk:  Clock Frequency (Hz)
+ *                    fnc:  Function Code (1 - Erase, 2 - Program, 3 - Verify)
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+static int32_t Init(uint32_t adr, uint32_t clk, uint32_t fnc)
+{
+
+    return (0);
+}
+
+/*
+ *  De-Initialize Flash Programming Functions
+ *    Parameter:      fnc:  Function Code (1 - Erase, 2 - Program, 3 - Verify)
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+static int32_t UnInit(uint32_t fnc)
+{
+
+    return (0);
+}
+
+/*
+ *  Erase complete Flash Memory
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+
+static int32_t EraseChip(void)
+{
+
+    return (0);
+}
+
+/*
+ *  Erase Sector in Flash Memory
+ *    Parameter:      adr:  Sector Address
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+static int32_t EraseSector(uint32_t adr)
+{
+	
+    return (0);
+}
+
+/*
+ *  Program Page in Flash Memory
+ *    Parameter:      adr:  Page Start Address
+ *                    sz:   Page Size
+ *                    buf:  Page Data
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+static int32_t ProgramPage(uint32_t addr, uint32_t sz, uint8_t* buf)
+{
+    int32_t result = 0;
+
+    return result;
+}
+
+/*
+ *  Read Page in Flash Memory
+ *    Parameter:      adr:  Page Start Address
+ *                    sz:   Page Size
+ *                    buf:  Page Data
+ *    Return Value:   0 - OK,  1 - Failed
+ */
+static int32_t ReadPage(uint32_t addr, uint32_t sz, uint8_t* buf)
+{
+    int32_t result = 0;
+
+    return result;
+}
+
+const  flash_algo_t  onchip_flash_device = {
+    .tFlashops.Init = Init,
+    .tFlashops.UnInit = UnInit,
+    .tFlashops.EraseChip = EraseChip,
+    .tFlashops.EraseSector = EraseSector,
+    .tFlashops.Program = ProgramPage,
+    .tFlashops.Read = ReadPage,
+    .ptFlashDev = &FlashDevice,
+};
+
