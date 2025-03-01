7fada475867032d2b88e54779d1a3a7f945b28a8
KK <17630350805@163.com>
2025-03-01 10:39:17 +0800

    添加 no semihosting 选项

--- template/no_semihosting.c
+++ template/no_semihosting.c
@@ -81,7 +81,6 @@ void _ttywrch(int ch)
 }
 #endif
 
-
 /* 当使用 AC6 开启 MicroLIB 时，若有使用 assert() 的需求，需要自己实现 __aeabi_assert() */
 #if __IS_COMPILER_ARM_COMPILER_6__ && defined(__MICROLIB)
 void __aeabi_assert(const char *chCond, const char *chLine, int wErrCode) 
@@ -92,5 +91,4 @@ void __aeabi_assert(const char *chCond, const char *chLine, int wErrCode)
     
     for (;;);
 }
-#endif
 #endif
